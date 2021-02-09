7cd6a0b55d1ba162af2d3f01ebe2fd51b3c22935
Michael-Zerghe <76479336+Michael-Zerghe@users.noreply.github.com>
2021-02-09 17:20:29 +0100

    update

--- script.js
+++ script.js
@@ -1,15 +1,235 @@
-let gallery = [
-    images/Start.svg,
-    images/tete.svg,
-    images/bras1.svg,
-    images/bras2.svg,
-    images/Jambe1.svg,
-    images/Pendufinal.svg,
-    images/Win.svg,
-    Images/Lose.svg,
-]
+(() => {
 
-let image = document.getElementById
-document.getElementById("class").addEventListener("click", function(){
+   /* let gallery = [
+        "images/Start.svg",
+        "images/tete.svg",
+        "images/bras1.svg",
+        "images/bras2.svg",
+        "images/Jambe1.svg",
+        "images/Pendufinal.svg",
+        "images/Win.svg",
+        "images/Lose.svg",
+    ]
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+   let affichage = []; 
 
-})
+    let a = 0;
+    let i = 0;
+
+
+    let j =0 ; 
+    let verif = "bonjour";
+ let word = verif.split(''); 
+
+
+    let img = document.getElementById("photo");
+    let letter = "";
+    let answer = document.getElementById("reponse");
+
+    function verify(){
+        if (verif.match(alphabet[i])){
+            
+        }
+        else {
+            img.src = gallery[i];
+        }
+    }
+
+    while(i<gallery.length){ 
+
+
+    document.getElementById("a").addEventListener("click", function(){
+        letter = document.getElementById("a").value;
+        a = alphabet[0];
+        verify();
+        
+
+    })
+
+    document.getElementById("b").addEventListener("click", function(){
+        letter = document.getElementById("b").value;
+        a = alphabet[1];
+        verify();
+        
+    })
+
+    document.getElementById("c").addEventListener("click", function(){
+        letter = document.getElementById("c").value;
+        a = alphabet[2];
+        verify();
+        
+    })
+
+    document.getElementById("d").addEventListener("click", function(){
+        letter = document.getElementById("d").value;
+        a = alphabet[3];
+        verify();
+        
+    })
+
+    document.getElementById("e").addEventListener("click", function(){
+        letter = document.getElementById("e").value;
+        a = alphabet[4];
+        verify();
+        
+    })
+
+    document.getElementById("f").addEventListener("click", function(){
+        letter = document.getElementById("f").value;
+        a = alphabet[5];
+        verify();
+        
+    })
+
+    document.getElementById("g").addEventListener("click", function(){
+        letter = document.getElementById("g").value;
+        a = alphabet[6];
+        verify();
+        
+    })
+
+    document.getElementById("h").addEventListener("click", function(){
+        letter = document.getElementById("h").value;
+        a = alphabet[7];
+        verify();
+        
+    })
+
+    document.getElementById("i").addEventListener("click", function(){
+        letter = document.getElementById("i").value;
+        a = alphabet[8];
+        verify();
+        
+    })
+
+    document.getElementById("g").addEventListener("click", function(){
+        letter = document.getElementById("j").value;
+        a = alphabet[9];
+        verify();
+        
+    })
+
+    document.getElementById("k").addEventListener("click", function(){
+        letter = document.getElementById("k").value;
+        a = alphabet[10];
+        verify();
+        
+    })
+
+    document.getElementById("l").addEventListener("click", function(){
+        letter = document.getElementById("l").value;
+        a = alphabet[11];
+        verify();
+        
+    })
+
+    document.getElementById("m").addEventListener("click", function(){
+        letter = document.getElementById("m").value;
+        a = alphabet[12];
+        verify();
+        
+    })
+
+    document.getElementById("n").addEventListener("click", function(){
+        letter = document.getElementById("n").value;
+        a = alphabet[13];
+        verify();
+        
+    })
+
+    document.getElementById("o").addEventListener("click", function(){
+        letter = document.getElementById("o").value;
+        a = alphabet[14];
+        verify();
+        
+    })
+
+    document.getElementById("p").addEventListener("click", function(){
+        letter = document.getElementById("p").value;
+        a = alphabet[15];
+        verify();
+        
+    })
+
+    document.getElementById("q").addEventListener("click", function(){
+        letter = document.getElementById("q").value;
+        a = alphabet[16];
+        verify();
+        
+    })
+
+    document.getElementById("r").addEventListener("click", function(){
+        letter = document.getElementById("r").value;
+        a = alphabet[17];
+        verify();
+        
+    })
+
+    document.getElementById("s").addEventListener("click", function(){
+        letter = document.getElementById("s").value;
+        a = alphabet[18];
+        verify();
+        
+    })
+
+    document.getElementById("t").addEventListener("click", function(){
+        letter = document.getElementById("t").value;
+        a = alphabet[19];
+        verify();
+        
+    })
+
+    document.getElementById("u").addEventListener("click", function(){
+        letter = document.getElementById("u").value;
+        a = alphabet[20];
+        verify();
+        
+    })
+
+    document.getElementById("v").addEventListener("click", function(){
+        letter = document.getElementById("v").value;
+        a = alphabet[21];
+        verify();
+        
+    })
+
+    document.getElementById("w").addEventListener("click", function(){
+        letter = document.getElementById("w").value;
+        a = alphabet[22];
+        verify();
+        
+    })
+
+    document.getElementById("x").addEventListener("click", function(){
+        letter = document.getElementById("x").value;
+        a = alphabet[23];
+        verify();
+        
+    })
+
+    document.getElementById("y").addEventListener("click", function(){
+        letter = document.getElementById("y").value;
+        a = alphabet[24];
+        verify();
+        
+    })
+
+    document.getElementById("z").addEventListener("click", function(){
+        letter = document.getElementById("z").value;
+        a = alphabet[25];
+        verify();
+        
+    })
+    i++
+}   */
+
+//recuperer lettre utilisateur (prompt)
+//comparer lettre au mot (boucle)
+//condition positive/negative
+//reset
+
+
+
+
+
+})();
