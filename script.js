5a403ea5837293d280553aba4b9ddf5608e0b3b8
Michael-Zerghe <76479336+Michael-Zerghe@users.noreply.github.com>
2021-02-09 09:07:14 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let pendu = [images/Start.svg,]
+let pendu = [
+    images/Start.svg,
+    images/tete.svg,
+    images/bras1.svg,
+    images/bras2.svg,
+    images/Jambe1.svg,
+    images/Pendufinal.svg,
+    images/Win.svg,
+    Images/Lose.svg,
+]
