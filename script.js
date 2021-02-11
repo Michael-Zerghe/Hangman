e7b0310124e030fe2ca813da256eb0ceaaa350d2
Michael-Zerghe <76479336+Michael-Zerghe@users.noreply.github.com>
2021-02-11 11:02:28 +0100

    js update

--- script.js
+++ script.js
@@ -3,25 +3,37 @@
 //comparer lettre au mot (boucle)
 //condition positive/negative
 //reset
-let word = "fuck";
+let word = "bonjour";
+let hidden = word.split('');
+let screen = [];
+let x = 0;
 let chances = 10;
 let letter = document.querySelectorAll("button");
-
+console.log(hidden);
+while(x<word.length){
+    screen.push('-');
+    x++;
+}
+document.getElementById("reponse").innerHTML = screen.join('');
 
 letter.forEach(p =>{
     p.addEventListener("click",function(){
-        if(word.match(p.innerHTML)){
-            console.log("oui");
+        if(hidden.includes(p.innerHTML)){
+            document.getElementById("reponse").innerHTML = p.innerHTML;
         }
-        else{
-            console.log("non");
+        else if(chances>0){
+            chances--;
+            document.getElementById("essais").innerHTML = "Numbers of tries left : " + chances;
         }
-        console.log(chances);
-        chances--;
-        
+        else if (chances ===0){
+            document.getElementById("essais").innerHTML = "You lose";
+            letter.forEach(k =>{
+                k.disabled = true;
+            })
+        } 
+        p.disabled = true;
+
     })
-
 })
-
 
 })();
