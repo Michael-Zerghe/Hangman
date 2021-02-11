d0b3d6d5876953683337641b3dd1674e39e95337
Michael-Zerghe <76479336+Michael-Zerghe@users.noreply.github.com>
2021-02-11 15:36:24 +0100

    final

--- script.js
+++ script.js
@@ -3,38 +3,74 @@
 //comparer lettre au mot (boucle)
 //condition positive/negative
 //reset
-let word = "art";
+gallery = [
+    "images/00.svg",
+    "images/01.svg",
+    "images/02.svg",
+    "images/03.svg",
+    "images/04.svg",
+    "images/05.svg",
+    "images/06.svg",
+    "images/Lose.svg",
+    "images/Win.svg",
+]
+
+let random = ["sangoku","vegeta","trunks","sangohan","broly","bulma","yamcha","freezer","nappa","cell","hit","jiren","babidi","karin"];
+
+let rng = Math.floor(Math.random()*random.length);
+let img = document.getElementById("photo");
+let word = random[rng];
 let hidden = word.split('');
 let screen = [];
 let x = 0;
-let i;
-let chances = 10;
+let galInd = 1;
+let chances = 7;
 let letter = document.querySelectorAll("button");
 while(x<word.length){
     screen.push('-');
     x++;
 }
 document.getElementById("reponse").innerHTML = screen.join('');
+document.getElementById("essais").innerHTML ="NUMBER OF TRIES LEFT : "+ chances;
 
 letter.forEach(p =>{
     p.addEventListener("click",function(){
-        if(hidden.includes(p.innerHTML)){
-           i = hidden.indexOf(p.innerHTML);
-           screen[i] = p.innerHTML;
-            document.getElementById("reponse").innerHTML = screen.join('');
-        }
-        else if(chances>0){
+       let i = 0;
+
+        hidden.forEach(m =>{
+            if(m === p.innerHTML){
+                screen[i]=hidden[i];
+                document.getElementById("reponse").innerHTML = screen.join('');
+            }
+        i++;
+
+        });
+
+        if(hidden.includes(p.innerHTML) === false){
             chances--;
-            document.getElementById("essais").innerHTML = "Numbers of tries left : " + chances;
+            document.getElementById("essais").innerHTML ="NUMBER OF TRIES LEFT : "+ chances;
+            img.src = gallery[galInd];
+            galInd++;
         }
-        else if (chances ===0){
-            document.getElementById("essais").innerHTML = "You lose";
-            letter.forEach(k =>{
-                k.disabled = true;
-            })
-        } 
+
         p.disabled = true;
+
+        if(screen.includes('-')===false){
+            letter.forEach(w => w.disabled = true);
+            document.getElementById("reponse").innerHTML = "YOU WIN";
+            img.src = gallery[8];
+        }
+        if (chances === 0){
+            letter.forEach(w => w.disabled = true);
+            document.getElementById("reponse").innerHTML = "YOU LOSE";
+        }
+        
     })
+
+    
+})
+document.getElementById("restart").addEventListener("click",function(){
+    document.location.reload();
 })
 
 })();
